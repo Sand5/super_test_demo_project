c0992873318ac976a659379305503ea21c0ac5c8
Sanderson <sanderson_imbeah5@hotmail.com>
2020-09-11 16:18:24 +0100

    Added new PUT tests to the suite of tests

---
+++ test/create_user.js
@@ -0,0 +1,28 @@
+const supertest = require("supertest");
+const expect = require("chai").expect;
+
+let baseURL = supertest("https://reqres.in");
+let list_user_endpoint = "/api/users";
+
+describe("POST Request", () => {
+  let post_resp;
+
+  it("Makes a POST call", async () => {
+    post_resp = await baseURL
+      .post(list_user_endpoint)
+
+      .type("form")
+      .send({
+        name: "Kwabena",
+        job: "automation",
+      })
+      .set("Accept", "/application/json/");
+
+    console.log(post_resp.body);
+  });
+
+  it("The response should be code 201", async () => {
+    expect(post_resp.status).to.equal(201);
+    console.log(post_resp.status);
+  });
+});
